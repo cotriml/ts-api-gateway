df03b616236a162cc032f1fcaf91e78df106071c
Lucas Cotrim <lucascotrim3@hotmail.com>
2021-06-26 18:55:26 -0300

    test: add ApiSampleRepository

---
+++ tests/infra/db/api-sample-repository.spec.ts
@@ -0,0 +1,23 @@
+import { ApiSampleRepository } from '@/infra/db'
+
+describe('ApiSampleRepository', () => {
+  const makeSut = (): ApiSampleRepository => {
+    return new ApiSampleRepository()
+  }
+
+  describe('loadByBaseUrl()', () => {
+    test('Should return a list of Apis on success', async () => {
+      const sut = makeSut()
+      const sampleBaseUrl = '/fake/v2'
+      const apiList = await sut.loadByBaseUrl(sampleBaseUrl)
+      expect(apiList).toBeTruthy()
+      expect(apiList.baseUrl).toBe(sampleBaseUrl)
+    })
+
+    test('Should return null if loadByBaseUrl fails', async () => {
+      const sut = makeSut()
+      const user = await sut.loadByBaseUrl('/fake/v3')
+      expect(user).toBeFalsy()
+    })
+  })
+})
