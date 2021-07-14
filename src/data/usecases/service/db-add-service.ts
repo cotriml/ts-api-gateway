a6f06bdac35689a46eed7081484b5f825f7df9d0
Lucas Cotrim <lucascotrim3@hotmail.com>
2021-07-13 23:18:15 -0300

    feat: add CheckServiceByBaseUrl validation on add and update service usecases

--- src/data/usecases/service/db-add-service.ts
+++ src/data/usecases/service/db-add-service.ts
@@ -1,12 +1,18 @@
-import { AddServiceRepository } from '@/data/protocols'
+import { AddServiceRepository, CheckServiceByBaseUrlRepository } from '@/data/protocols'
 import { AddService } from '@/domain/usecases'
 
 export class DbAddService implements AddService {
   constructor (
-    private readonly addServiceRepository: AddServiceRepository
+    private readonly addServiceRepository: AddServiceRepository,
+    private readonly checkServiceByBaseUrlRepository: CheckServiceByBaseUrlRepository
   ) { }
 
   async add (params: AddService.Params): Promise<AddService.Result> {
+    const exists = await this.checkServiceByBaseUrlRepository.checkByBaseUrl(params.baseUrl)
+
+    if (exists) {
+      return null
+    }
     return await this.addServiceRepository.add(params)
   }
 }
--- src/data/usecases/service/db-update-service.ts
+++ src/data/usecases/service/db-update-service.ts
@@ -1,12 +1,19 @@
-import { UpdateServiceRepository } from '@/data/protocols'
+import { CheckServiceByBaseUrlRepository, UpdateServiceRepository } from '@/data/protocols'
 import { UpdateService } from '@/domain/usecases'
 
 export class DbUpdateService implements UpdateService {
   constructor (
-    private readonly updateServiceRepository: UpdateServiceRepository
+    private readonly updateServiceRepository: UpdateServiceRepository,
+    private readonly checkServiceByBaseUrlRepository: CheckServiceByBaseUrlRepository
+
   ) { }
 
   async update (params: UpdateService.Params): Promise<UpdateService.Result> {
+    const exists = await this.checkServiceByBaseUrlRepository.checkByBaseUrl(params.baseUrl)
+
+    if (exists) {
+      return null
+    }
     return await this.updateServiceRepository.update(params)
   }
 }
