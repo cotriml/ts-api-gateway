8a557f445915e25338b0e97ede8dbda0b4fcf07f
Lucas Cotrim <lucascotrim3@hotmail.com>
2021-07-26 21:30:04 -0300

    feat: add DeleteServiceController

---
+++ src/presentation/controllers/service/delete-user-controller.ts
@@ -0,0 +1,30 @@
+import { Controller, HttpResponse } from '@/presentation/protocols'
+import { InvalidParamError } from '@/presentation/errors'
+import { badRequest, noContent, serverError } from '@/presentation/helpers'
+import { DeleteService } from '@/domain/usecases'
+
+export class DeleteServiceController implements Controller {
+  constructor (
+    private readonly deleteService: DeleteService
+  ) { }
+
+  async handle (request: DeleteServiceController.Request): Promise<HttpResponse> {
+    try {
+      const { serviceId } = request
+
+      const result = await this.deleteService.delete(serviceId)
+      if (!result) {
+        return badRequest(new InvalidParamError('serviceId'))
+      }
+      return noContent()
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
+
+export namespace DeleteServiceController {
+  export type Request = {
+    serviceId: string
+  }
+}
--- src/presentation/controllers/service/index.ts
+++ src/presentation/controllers/service/index.ts
@@ -1,1 +1,2 @@
 export * from './add-service-controller'
+export * from './delete-user-controller'
